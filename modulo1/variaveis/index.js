db19393f310641ae473c3f68fdeb9c6c7095a9f5
Mauricio da Rosa Gonçalves <mauriciorg89@hotmail.com>
2022-04-12 22:10:49 -0300

    exercicio pela metade

--- modulo1/variaveis/index.js
+++ modulo1/variaveis/index.js
@@ -1,7 +1,54 @@
-let a = 10
-let b = 10
+//Exercícios de interpretação de código
+//let a = 10
+//let b = 10
 
-console.log(b)
+//console.log(b)
 
-b = 5
-console.log(a, b)
+//b = 5
+//console.log(a, b)
+// 10,5
+
+/*2let a = 10
+let b = 20
+c = a
+b = c
+a = b
+console.log(a, b, c)
+// 10,10,10
+
+ b//let p = prompt("Quantas horas você trabalha por dia?")
+//let t = prompt("Quanto você recebe por dia?")
+//alert(`Voce recebe ${t/p} por hora`)
+p= horasTrabalhadas
+t= valorDiaTrabalhado*/
+
+// Exercícios de escrita de código
+/*A let nome 
+let idade
+
+B console.log(typeof nome)
+console.log(typeof idade)
+// D Resultado undefined porque nao foi atribuido nenhum valor ao nome e ao let
+
+
+ nome = prompt("Qual é o seu nome?")
+ idade = prompt("Qual é a sua idade?")
+
+ nome = prompt("Qual é o seu nome?")
+ idade = prompt("Qual é a sua idade?")
+
+f console.log("notei que ele pergunta o nome a idade a cada vez que a pagina é acessada")
+gprompt("Olá!", nome, "vc tem", idade, "anos")
+console.log(nome, idade)*/
+
+
+
+let = lasanha
+let = pimenta
+let = churrasco
+
+lasanha - prompt("voce gosta de lasanha? - Sim ou Nao")
+pimenta - prompt("voce gosta de pimenta? - Sim ou Nao")
+churrasco - prompt("voce gosta de churrasco? - Sim ou Nao")
+
+console.log(lasanha,pimenta,churrasco)
