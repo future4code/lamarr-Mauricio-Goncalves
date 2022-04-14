5603e6443592b762efc38821b44c451909582778
Mauricio da Rosa Gonçalves <mauriciorg89@hotmail.com>
2022-04-13 22:14:03 -0300

    atividade operadores

---
+++ modulo1/operadores/index.js
@@ -0,0 +1,26 @@
+// 1 Leia o código abaixo. Indique todas as mensagens impressas no console, SEM EXECUTAR o programa.
+/*const bool1 = true
+const bool2 = false
+const bool3 = !bool2
+
+let resultado = bool1 && bool2
+console.log("a. ", resultado) // false
+
+resultado = bool1 && bool2 && bool3 
+console.log("b. ", resultado) //false
+
+resultado = !resultado && (bool1 || bool2) 
+console.log("c. ", resultado) //true
+
+console.log("d. ", typeof resultado) */
+
+//2Seu colega se aproxima de você falando que o código dele não funciona como devia.  Vamos ajudá-lo: consegue perceber algum problema? O que será impresso no console? 
+
+let primeiroNumero = prompt("Digite um numero!")
+let segundoNumero = prompt("Digite outro numero!")
+
+soma= primeiroNumero + segundoNumero
+
+console.log(soma)
+
+
