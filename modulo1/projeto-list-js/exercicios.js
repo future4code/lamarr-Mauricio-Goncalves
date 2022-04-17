04cee015b6368c2f13f7473617a95d7ec1ac23f1
Mauricio da Rosa Gonçalves <mauriciorg89@hotmail.com>
2022-04-17 10:03:50 -0300

    Comecei o projeto

---
+++ modulo1/projeto-list-js/exercicios.js
@@ -0,0 +1,135 @@
+// EXEMPLOS DE IMPLEMENTAÇÃO ---------------------------------------------------------------
+
+// EXERCÍCIO 0A
+function soma(num1, num2) {
+  // implemente sua lógica aqui
+  return num1 + num2
+}
+
+// EXERCÍCIO 0B
+function imprimeMensagem() {
+  // implemente sua lógica aqui
+  const mensagem = prompt('Digite uma mensagem!')
+
+  console.log(mensagem)
+}
+
+// EXERCÍCIOS PARA FAZER ------------------------------------------------------------------
+
+// EXERCÍCIO 01
+function calculaAreaRetangulo() {
+  // implemente sua lógica aqui
+  let altura = Number(prompt("escreva uma altura"))
+  let largura = Number(prompt("escreva uma largura"))
+  console.log( altura * largura)
+
+}
+
+// EXERCÍCIO 02
+function imprimeIdade() {
+  // implemente sua lógica aqui
+  let anoNascimento = Number(prompt("digite ano do seu Nascimento"))
+  let anoAtual = Number(prompt("Digite ano autal"))
+  console.log(anoNascimento - anoAtual)
+  
+
+}
+
+// EXERCÍCIO 03
+function calculaIMC(peso, altura) {
+  // implemente sua lógica aqui
+  
+  
+}
+
+// EXERCÍCIO 04
+function imprimeInformacoesUsuario() {
+  // implemente sua lógica aqui
+  // "Meu nome é NOME, tenho IDADE anos, e o meu email é EMAIL."
+  const nome = prompt('Digite seu nome')
+  const idade = prompt('Digite sua idade')
+  const email = prompt('Digite seu email')
+  console.log(`Meu nome é ${nome}, tenho ${idade} anos, e o meu email é ${email}.`)
+
+}
+
+// EXERCÍCIO 05
+function imprimeTresCoresFavoritas() {
+  // implemente sua lógica aqui
+let cor1 = prompt("digite uma cor favorita")
+let cor2 = prompt("digite outra cor favorita")
+let cor3 = prompt("digite mais uma cor favorita")
+console.log(`${cor1}`)
+console.log(`${cor2}`)
+console.log(`${cor3}`)
+
+}
+
+// EXERCÍCIO 06
+function retornaStringEmMaiuscula(string) {
+  // implemente sua lógica aqui
+  let texto = prompt("digite uma frase")
+  console.log(texto .toUpperCase)
+  
+
+
+}
+
+// EXERCÍCIO 07
+function calculaIngressosEspetaculo(custo, valorIngresso) {
+  // implemente sua lógica aqui
+let valorIngresso = Number(prompt("Digite o valor do ingresso"))
+let custo = Number(prompt("Digite o custo do show"))
+let valor = custo / valorIngresso
+console.log(valor)
+
+
+}
+
+// EXERCÍCIO 08
+function checaStringsMesmoTamanho(string1, string2) {
+  // implemente sua lógica aqui
+
+}
+
+// EXERCÍCIO 09
+function retornaPrimeiroElemento(array) {
+  // implemente sua lógica aqui
+
+}
+
+// EXERCÍCIO 10
+function retornaUltimoElemento(array) {
+  // implemente sua lógica aqui
+
+}
+
+// EXERCÍCIO 11
+function trocaPrimeiroEUltimo(array) {
+  // implemente sua lógica aqui
+
+}
+
+// EXERCÍCIO 12
+function checaIgualdadeDesconsiderandoCase(string1, string2) {
+  // implemente sua lógica aqui
+
+}
+
+// EXERCÍCIO 13
+function checaRenovacaoRG() {
+  // implemente sua lógica aqui
+
+}
+
+// EXERCÍCIO 14
+function checaAnoBissexto(ano) {
+  // implemente sua lógica aqui
+
+}
+
+// EXERCÍCIO 15
+function checaValidadeInscricaoLabenu() {
+  // implemente sua lógica aqui
+
+}
