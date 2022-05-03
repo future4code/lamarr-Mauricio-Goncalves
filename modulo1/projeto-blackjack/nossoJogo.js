5b0e332f996947ce5fa92ef4f6b0010c239ee7f0
Mauricio da Rosa Gonçalves <mauriciorg89@hotmail.com>
2022-05-03 18:49:13 -0300

    Blackjack jogo

--- modulo1/projeto-blackjack/nossoJogo.js
+++ modulo1/projeto-blackjack/nossoJogo.js
@@ -10,13 +10,100 @@
  * 
  * 
  */
-console.log("Bem vindo ao jogo!")
-   if (confirm("Você quer iniciar o jogo")){
-      console.log("Nova rodada!")
+    
+console.log("Bem vindo(a) ao jogo de Blackjack!")
+   if (confirm("Você quer iniciar uma nova rodada?")){
+
+      let carta1 = comprarCarta()
+      let carta2 = comprarCarta()
+      let cartaPc1 = comprarCarta()
+      let cartaPc2 = comprarCarta()
+      let somaValor = carta1.valor + carta2.valor
+      let somaValor2 = cartaPc1.valor + cartaPc2.valor
+    
+      console.log("Usuario - cartas:", carta1.texto ,"+", carta2.texto, "-", somaValor)
+      console.log("Computador - cartas:", cartaPc1.texto, "+", cartaPc2.texto ,"-",somaValor2) 
+   if(somaValor === somaValor2 ){
+      console.log("Empate!")}
+   if(somaValor > somaValor2){
+      console.log("Usuario Ganhou!")
    }
-   else { console.log("O jogo acabou!")
+   if(somaValor < somaValor2){
+      console.log("Computador Ganhou!")
+   }
+   
+      }
+
+     else{
+        console.log("O jogo Acabou")
    }
 
-   const carta = comprarCarta();
-   console.log(carta.texto)
-   console.log(carta.valor)
+   
+
+  
+  
+
+  
+  
+  
+  
+  
+  
+  
+  
+  // const resultadoUsuario = carta1.texto + carta2.valor
+      // const resultadoPC = cartaPc1.texto + cartaPc2.valor
+      // console.log(`Usuario - cartas:, ${carta1.valor + carta2.texto}`),
+      // console.log(`Computador - cartas: ${cartaPc1.valor +cartaPc2.texto} -`) 
+      // // console.log("Computador - cartas:",resultadoPC, "-") 
+     
+      //  const resultadoUsuario = [carta1.texto + carta2.texto]
+      //  const resultadoPC = [cartaPc1.texto + cartaPc2.texto]
+  
+  
+  
+  
+  
+   // if(resultadoUsuario > 21){
+   //    console.log("Empate");}
+   // } else if(resultadoUsuario < 21 !== resultadoPC <=21){
+   //    console.log("Usuario Ganhou!")
+   // }
+   
+
+   
+   
+  
+   
+  
+
+
+
+
+
+
+
+
+
+
+
+
+ //    let comprarCarta = carta.texto
+   // let comprarCarta2 = carta.texto
+   // let pcCompraCarta = carta.texto
+   // let pcCompraCarta2 = carta.texto
+   // if (comprarCarta + comprarCarta2 <= 21 && pcCompraCarta + pcCompraCarta2 >=21 )
+   // console.log(carta.texto)
+   // console.log(carta.valor)
+   // console.log("Você Ganhou!")
+
+
+   // const carta = comprarCarta()
+
+// if(carta.valor, carta.texto === 21){
+// console.log("Voce Ganhou")
+// }
+// else (confirm("Você quer comprar uma carta?"))
+// {
+
+// }
