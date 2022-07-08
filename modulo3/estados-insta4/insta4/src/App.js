e695354d4fbe7b228660d5b1977619dbd0f30455
Mauricio da Rosa Gonçalves <mauriciorg89@hotmail.com>
2022-07-07 22:00:07 -0300

    insta lista2

--- modulo3/estados-insta4/insta4/src/App.js
+++ modulo3/estados-insta4/insta4/src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Post from './components/Post/Post';
 
 
 function App() {
-  const postagens = [
-    
-      {nomeUsuario:'paulinha',
-      fotoUsuario: 'https://picsum.photos/10/10',
-      fotoPost: 'https://picsum.photos/200/150'},
+ 
+  const [inputPostagem, setInputPostagem] = useState("")
+  const [inputFoto, setInputFoto]= useState("")
+  const [inputTexto, setInputTexto]= useState("")
+  const [postagem, setPostagem] = useState([
 
-      {nomeUsuario:'Fernanda',
-      fotoUsuario: 'https://picsum.photos/20/20',
-      fotoPost: 'https://picsum.photos/100/110'},
+    {nomeUsuario:'paulinha',
+    fotoUsuario: 'https://picsum.photos/10/10',
+    fotoPost: 'https://picsum.photos/200/150'},
+  
+    {nomeUsuario:'Fernanda',
+    fotoUsuario: 'https://picsum.photos/20/20',
+    fotoPost: 'https://picsum.photos/100/110'},
+  
+    {nomeUsuario:'Juliana',
+    fotoUsuario: 'https://picsum.photos/30/30',
+    fotoPost: 'https://picsum.photos/300/151'},
 
-      {nomeUsuario:'Juliana',
-      fotoUsuario: 'https://picsum.photos/30/30',
-      fotoPost: 'https://picsum.photos/300/151'},
-  
-    
-  ]
-  
-  let postagem = postagens.map((post,index) => {
+
+
+  ])
+
+  const handleInputPostagem = (e) => {
+    setInputPostagem(e.target.value)
+  }
+  const handleInputFoto = (e) => {
+    setInputFoto(e.target.value)
+  }
+  const handleInputTexto = (e) => {
+    setInputTexto(e.target.value)
+  }
+  const addPostagem = (e) => {
+    e.preventDefault();
+
+    const novaPostagem = {nomeUsuario: inputPostagem, fotoUsuario: inputFoto, fotoPost: inputTexto}
+    const novaListaPostagem = [...postagem, novaPostagem]
+    setPostagem(novaListaPostagem)
+  }
+
+
+  let listadePostagem = postagem.map((post,index) => {
     return (
       <Post key={index} nomeUsuario={post.nomeUsuario}
       fotoUsuario={post.fotoUsuario}
@@ -30,8 +53,26 @@ function App() {
   });
   return (
    <div>
-    {postagem}
-    {console.log(postagem)}
+    <form>
+      <label>Nova Postagem</label>
+      <input
+        placeholder='Insira seu Nome'
+        value={inputPostagem}
+        onChange={handleInputPostagem}/>
+         <input
+        placeholder='Insira sua Foto'
+        value={inputFoto}
+        onChange={handleInputFoto}/>
+         <input
+        placeholder='Insira o Texto'
+        value={inputTexto}
+        onChange={handleInputTexto}/>
+        <button onClick={addPostagem}>Adicionar</button>
+
+
+    </form>
+    {listadePostagem}
+    {console.log(listadePostagem)}
    </div>
 
 
