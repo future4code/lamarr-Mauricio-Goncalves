4934dcb99d0e0451b3a0f16b357615b96b678858
Mauricio da Rosa Gonçalves <mauriciorg89@hotmail.com>
2022-08-04 23:21:29 -0300

    projeto estilizado

--- modulo3/projeto-astromatch/astromatch/src/components/TelaPrincipal/Styled.js
+++ modulo3/projeto-astromatch/astromatch/src/components/TelaPrincipal/Styled.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const ContainerPrincipal = styled.div `
     width: 400px;
-    height: 600px;
+    height: 670px;
     position: fixed;
     border: 1px solid #0000;
     background-color: #836fff;
@@ -12,9 +12,61 @@ export const ContainerPrincipal = styled.div `
     flex-direction: column;
     border-radius: 30px;
     align-items: center;
+    `
+export const ContaionerBio = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+
+    p{
+        font-size: 20px;
+        font-weight: bold;
+        color: white;
+    }
+`
+export const ContainerMatch = styled.div`
+    display: flex;
+    justify-content: space-evenly;
+    
+`
+
+
+
+
+
+ export const Butao = styled.button`
+  width: 50px;
+    border: none;
+    background-color: #836fff;
+    margin-left: 1rem;
+ 
+ img {
+    width: 30px;
+ }
+ `
+export const Imagem = styled.img `
+    width: 300px;
+    height: 300px;
+    padding: 10px;
+    `
+
+
+export const Buttons = styled.button `
+    width: 50px;
+    border: none;
+    background-color: #836fff;
+    margin-right: 2rem;
 
     img{
-        padding: 10px;
-        width: 300px;
+    max-width: 50px;
     }
+`
+
+export const ContainerButtons = styled.div `
+    display :flex ;
+    justify-content: space-between;
+    margin-top: 3rem ;
+    margin-left: 2rem;
+   
 `
--- modulo3/projeto-astromatch/astromatch/src/components/TelaPrincipal/TelaPrincipal.js
+++ modulo3/projeto-astromatch/astromatch/src/components/TelaPrincipal/TelaPrincipal.js
@@ -1,50 +1,47 @@
-import React, {useState, useEffect} from "react"
+import React, { useState, useEffect } from "react"
 import axios from "axios"
-import { ContainerPrincipal } from "./Styled"
+import { Buttons, ContainerPrincipal, Imagem, ContainerButtons, ContaionerBio, Butao, ContainerMatch } from "./Styled"
 
 
 
-export const TelaPrincipal = ()=> {
-   const[persons, setPersons] = useState([])
-   
-   
-   useEffect(() => {
-    getUsers();
-  }, []);
-   const url = 'https://us-central1-missao-newton.cloudfunctions.net/astroMatch/:Mauricio-Goncalves-Lammar/person'             
-       
+export const TelaPrincipal = () => {
+    const [persons, setPersons] = useState([])
 
-const getUsers=() =>{
-    axios.get(url)
-    .then((response)=>{
-        setPersons([response.data.profile])
-    }).catch((err)=>{
-        console.log(err);
-    })
+
+    useEffect(() => {
+        getUsers();
+    }, []);
+    const url = 'https://us-central1-missao-newton.cloudfunctions.net/astroMatch/:Mauricio-Goncalves-Lammar/person'
+
+
+    const getUsers = () => {
+        axios.get(url)
+            .then((response) => {
+                setPersons([response.data.profile])
+            }).catch((err) => {
+                console.log(err);
+            })
+    }
+
+    return (
+        <>
+            {persons.map((item) => {
+                return (
+                    <ContainerPrincipal>
+                        <ContainerMatch>
+                        <h1>Astromatch</h1>
+                        <Butao> <img src="https://cdn-icons-png.flaticon.com/128/3079/3079258.png" alt="" /></Butao></ContainerMatch>
+                        <Imagem src={item.photo} alt="" />
+                        <ContaionerBio>
+                          <p>{item.name + ", "}  
+                             {item.age} </p> 
+                          <p>{item.bio}</p> </ContaionerBio>
+                        <ContainerButtons>
+                            <Buttons><img src="https://cdn-icons-png.flaticon.com/128/458/458594.png" /></Buttons>
+                            <Buttons> <img src="https://cdn-icons.flaticon.com/png/128/3669/premium/3669698.png?token=exp=1659656029~hmac=317d1243cb3343979ed0af87787c148a" alt="" /></Buttons></ContainerButtons>
+                    </ContainerPrincipal>
+                )
+            })}
+        </>
+    )
 }
-
-   return(
-    <>
-        {persons.map((item)=>{
-            return(
-                <ContainerPrincipal>
-                    <img src={item.photo} alt="" />
-                    {item.name}
-                    {item.age}
-                </ContainerPrincipal>
-             )
-        })}
-         
-
-
-
-
-
-
-
-
-
-
-    </>
-   ) 
-   }
