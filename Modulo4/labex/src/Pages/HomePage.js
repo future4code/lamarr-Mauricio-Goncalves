aad8989c57a47f8d6ec0bfb0a0754e768b6e1eb3
Mauricio da Rosa Gonçalves <mauriciorg89@hotmail.com>
2022-08-11 22:48:03 -0300

    projeto parte 2

--- Modulo4/labex/src/Pages/HomePage.js
+++ Modulo4/labex/src/Pages/HomePage.js
@@ -9,7 +9,7 @@ export const HomePage =() => {
         <div>
             <h1>HomePage</h1>
             <button onClick={()=>{MyRoute.goToPageAdmin(navigate)}}>Area Administrativa</button>
-            <button onClick={()=>{MyRoute.goToListTripsPage(navigate)}}>2</button>
+            <button onClick={()=>{MyRoute.goToListTripsPage(navigate)}}>Lista de Viagens</button>
             <button onClick={()=>{MyRoute.goToApplicationFormPage(navigate)}}>3</button>
             <button onClick={()=>{MyRoute.goToTripDetailsPage(navigate)}}>4</button>
             <button onClick={()=>{MyRoute.goToCreateTripPage(navigate)}}>5</button>
--- Modulo4/labex/src/Pages/ListTripsPage.js
+++ Modulo4/labex/src/Pages/ListTripsPage.js
@@ -1,36 +1,107 @@
-import React, {useState} from "react";
+import React, { useState,useEffect } from "react";
 import {useNavigate} from "react-router-dom"
-import axios from 'axios'
 import * as MyRoute from '../router/codinator'
+import { useRequestData } from "../Hooks/useRequestData";
+import { BASE_URL } from "../constants/constants";
+
+
 
 export const ListTripsPage =() => {
-    const [trips, setTrips] = useState([])
     const navigate=useNavigate()
-    
+    const [dataUser,isLoaddingUser,erroUser]=useRequestData(`${BASE_URL}trips`)
 
-    const url = 'https://us-central1-labenu-apis.cloudfunctions.net/labeX/:mauricio-goncalves-lamarr/trips'
-
-
-    const getUsers = () => {
-        axios.get(url)
-            .then((response) => {
-                console.log(response)
-            }).catch((err) => {
-                console.log(err);
-            })
-    }
-
-
-
-
-
+    const user=dataUser&&dataUser.trips.map((trip)=>{
+        return<li>{trip.name}</li>
+        })
     return (
         <>
-            <h1>Lista de viagems</h1>
-           <button onClick={()=>{MyRoute.goToBack(navigate)}}>voltar</button>
+     <h1>Lista de viagems</h1>
+         {isLoaddingUser&&"...Carregando!!! ...."}
+        <ul>
+        {!isLoaddingUser&&dataUser&&user}
+        </ul>
+        {!isLoaddingUser&&!dataUser&&erroUser}
+        <button onClick={()=>{MyRoute.goToBack(navigate)}}>voltar</button>
         </>
     )
-
-
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// export const ListTripsPage =()=>{
+//     const navigate=useNavigate();
+//     const [listTrips, setListTrips] =useState([]);
+   
+//     useEffect(()=>{
+//         getTrips()
+//     },[navigate])
+
+
+//         const getTrips = ()=>{axios.get(`${BASE_URL}`).then((response)=>{
+//                     setListTrips([response.data.trips])
+//                     console.log(response.data.trips);
+//                 }).catch((error)=>{
+//                     console.log(error)  })}
+    
+
+//     listTrips.map((trip)=>{
+//         return(<h1>{trip.planet}</h1>)
+
+//     })                
+//   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
