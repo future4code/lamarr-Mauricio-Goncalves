cca4305814d96808c1a700acda5b905875a1a795
Mauricio da Rosa Gonçalves <mauriciorg89@hotmail.com>
2022-10-29 20:47:03 -0300

    exercicio 5, 6, 7 ,8 prontos

--- modulo5/aprofundamento-express/src/data.ts
+++ modulo5/aprofundamento-express/src/data.ts
@@ -22,3 +22,31 @@ export const afazeres: ToDo[] =[
     completed: false    
     }
 ]
+
+export type Post = {
+    userId: number,
+    id: number,
+    title: string,
+    body: string
+}
+
+export const posts: Post[] = [
+    {
+      userId: 1,
+      id: 11,
+      title: "Texto sobre quando nao sabe o que colocar",
+      body: "Não saber o que colocar em textos nos exercicios é uma coisa muito complicada"
+    },
+    {
+      userId: 2,
+      id: 22,
+      title: "Imagina em 2 textos",
+      body: "Se em um texto ja é dificil imagina fazer dois textos"
+    },
+    {
+      userId: 3,
+      id: 33,
+      title: "Tres fica mais complicado",
+      body: " Fazer tres textos para preencher exericicios as vezes é meio chato "
+    },
+]
--- modulo5/aprofundamento-express/src/index.ts
+++ modulo5/aprofundamento-express/src/index.ts
@@ -1,4 +1,4 @@
-
+import { ToDo, afazeres, Post, posts} from "./data"
 import  express, { Request, Response} from 'express'
 
 import cors from 'cors'
@@ -35,3 +35,62 @@ app.get("/todos/completed/:isCompleted", (req:Request, res: Response)=>{
     res.status(201).send("Pong!")
 
 })
+//exe5
+
+app.post("/todos", (req, res) => {
+  const userId = req.body.userId
+  const title = req.body.title
+
+  const newTodo: ToDo = {
+    userId,
+    id: Date.now(),
+    title,
+    completed: false
+  }
+
+  afazeres.push(newTodo)
+
+  res.send(afazeres)
+})
+//exe6
+app.put("/todos/:id/completed", (req, res) => {
+
+      const id = Number(req.params.id)
+    
+      for (let todo of afazeres) {
+        if (todo.id === id) {
+          todo.completed = !todo.completed
+        }
+      }
+    
+      res.send(afazeres)
+
+
+//exe7
+
+app.delete("/todos/:id", (req, res) => {
+
+      const id = Number(req.params.id)
+    
+      for (let i = 0; i < afazeres.length; i++) {
+        if (afazeres[i].id === id) {
+          afazeres.splice(i, 1)
+        }
+      }
+    
+      res.send(afazeres)
+    })
+    //exe8
+    app.get("/users/:userId/todos", (req, res) => {
+      const userId = Number(req.params.userId)
+    
+      const result = []
+    
+      for (let todo of afazeres) {
+        if (todo.userId === userId) {
+          result.push(todo)
+        }
+      }
+    
+      res.send(result)
+    }),
